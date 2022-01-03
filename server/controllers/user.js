6498176e7a2a68d31435eda551a1e08af0039dfa
evandrarf <evandraraditya049@gmail.com>
2022-01-03 21:17:33 +0700

    Update v3.0.01

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -15,7 +15,7 @@ export const signin = async (req, res) => {
 
     if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials." });
 
-    const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, "test", { expiresIn: "1h" });
+    const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, "test", { expiresIn: "8h" });
 
     res.status(200).json({ result: existingUser, token });
   } catch (error) {
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,13 @@
 import express from "express";
 
-import { getPosts, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
+import { getPostsBySearch, getPosts, createPost, updatePost, deletePost, likePost, getPost } from "../controllers/posts.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
+router.get("/search", getPostsBySearch);
 router.get("/", getPosts);
+router.get("/:id", getPost);
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
 router.delete("/:id", auth, deletePost);
